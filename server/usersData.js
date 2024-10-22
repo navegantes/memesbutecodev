06ad965e06bb94aac4e58ee43ad19947eacbd828
navegantes <raphaelnavegantes@gmail.com>
2024-10-22 03:18:56 -0300

    chore: update users data

--- server/usersData.js
+++ server/usersData.js
@@ -37,22 +37,69 @@ export const usersList = [
 
 export const testimonies = [
   {
-    login: "jayp-xgh",
-    id: 90224100,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
+    login: "EricksonLuiz",
+    id: 101117503,
+    testimony: "Erickson Luiz Delfino está digitando...",
   },
   {
-    login: "brunofunnie",
-    id: 5279392,
+    login: "matheusvidaltenorio",
+    id: 83835643,
     testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
+      "Êh macho que é isso aí macho. Qual esse servidor aí macho? Éh macho esse servidor é bom viu macho.",
   },
   {
-    login: "deablofk",
-    id: 51531895,
+    login: "CodeMoreira",
+    id: 78581400,
     testimony:
-      "Esse server é baum, só não é tão baum assim pq ninguém me dá emprego. Vocês já viram os novos atalhos que eu configurei no meu novo teclado?",
+      "Fala meu bom. Tudo certinho, tudo belezinha? Essa comunidade do Buteco dos Dev óh é daorinha recomendo.",
+  },
+  {
+    login: "juniorx57",
+    id: 130268695,
+    testimony:
+      "Olha esses caras, olha esses caras. Aqui é o melhor lugar pra puxar um cafézinho.",
+  },
+  {
+    login: "Manuel-Antunes",
+    id: 57446204,
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum massa in lorem commodo, sit amet porta arcu ullamcorper. Aenean accumsan eget elit in vulputate. Praesent rutrum ultrices arcu, id vestibulum dui fermentum vitae. Vivamus varius lorem nibh, non molestie nunc feugiat nec. Pellentesque orci velit, molestie quis arcu in, finibus scelerisque lectus. Phasellus feugiat pretium turpis, sit amet condimentum odio consectetur eget. Sed elementum sed nibh ut molestie. Vivamus augue lorem, malesuada imperdiet ante et, faucibus bibendum eros. Quisque suscipit tempor arcu sed rutrum. Etiam ultrices venenatis lobortis. Vivamus cursus, tortor ac laoreet commodo, mi libero varius purus, id malesuada felis enim at nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas interdum ipsum sed varius venenatis. Praesent vitae orci in mauris varius facilisis ut in tortor. Nam bibendum, nulla nec laoreet facilisis, lorem diam venenatis elit, et semper est lacus ut sem. Pellentesque dapibus scelerisque felis, sit amet venenatis est aliquet ac. Integer finibus, nibh non congue faucibus, dolor purus consectetur est, nec euismod neque elit vel sem. Nullam finibus tristique placerat.",
+  },
+  {
+    login: "thassiadevilart",
+    id: 46179885,
+    testimony:
+      "Gente é o melhor servidor dev do discord, aqui na Alemanha, na Baviera a gente não têm um servidor assim, com tanto conteúdo!",
+  },
+  {
+    login: "joaofolle",
+    id: 13815907,
+    testimony:
+      "Olá rapazes! Deixa eu te falar o servidor do Buteco é o melhor neah, tá ligaduo",
+  },
+  {
+    login: "lucas-koji",
+    id: 121801631,
+    testimony:
+      "Olha, não queria falar nada não, mas o Buteco dos Devs só não é melhor que a 42.",
+  },
+  {
+    login: "giomartinsdev",
+    id: 125399281,
+    testimony:
+      "Fala aí rapaziada. Esse servidor é manero hein. Todo jovem aprendiz deveria fazer parte mané!",
+  },
+  {
+    login: "astahjmo",
+    id: 49465538,
+    testimony:
+      "É uma comunidade de desenvolvedores bastante aberta a novas ideias e tecnologias, eles deixam até eu andar com uma faca na minha mochila.",
+  },
+  {
+    login: "jaumdev1",
+    id: 56412536,
+    testimony:
+      "Ôh galera cês sabiam que têm um estudo que fala que a interação de devs de várias senioridades em um servidor como o discord falicita a capacidade de apendizado?",
   },
   {
     login: "iankyy",
@@ -73,16 +120,22 @@ export const testimonies = [
       "Bota o hino do Curintia! Vocês viram que saiu uma versão do hino do corintias no Quarkus?!",
   },
   {
-    login: "bloiseleo",
-    id: 105981050,
+    login: "felipepassion",
+    id: 29386600,
     testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
+      "Aí lek saca só essa funcionalidade no Blazor que dá pra implementar no servidor do Buteco dos Devs, vaificar daora, serio mesmo.",
   },
   {
-    login: "jaumdev1",
-    id: 56412536,
+    login: "jayp-xgh",
+    id: 90224100,
     testimony:
-      "Ôh galera cês sabiam que têm um estudo que fala que a interação de devs de várias senioridades em um servidor como o discord falicita a capacidade de apendizado?",
+      "Ah vão dormir seus mama hola, o que cês tá mole. O servidor não é pra vocês ficar zoando não, nossa comunidade é a melhor do Discord, ná onde que têm outro assim?.",
+  },
+  {
+    login: "deablofk",
+    id: 51531895,
+    testimony:
+      "Esse server é baum, só não é tão baum assim pq ninguém me dá emprego. Vocês já viram os novos atalhos que eu configurei no meu novo teclado?",
   },
   {
     login: "cafe-e-codigo",
@@ -91,112 +144,76 @@ export const testimonies = [
       "Olha que loco né cara. Na época que eu começei a programar não tinha uma comunidade tão interessante como o Buteco dos Devs.",
   },
   {
-    login: "djonatah",
-    id: 22297855,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "JairAssisDev",
-    id: 63867699,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "astahjmo",
-    id: 49465538,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "AninhaPardini",
-    id: 108814319,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "giomartinsdev",
-    id: 125399281,
-    testimony:
-      "Fala aí rapaziada. Esse servidor é manero hein. Todo jovem aprendiz deveria fazer parte mané!",
-  },
-  {
-    login: "Gus-fs",
-    id: 104308051,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "lucas-koji",
-    id: 121801631,
-    testimony:
-      "Olha, não queria falar nada não, mas o Buteco dos Devs só não é melhor que a 42.",
-  },
-  {
-    login: "joaofolle",
-    id: 13815907,
-    testimony:
-      "Olá rapazes! Deixa eu te falar o servidor do Buteco é o melhor neah, tá ligaduo",
-  },
-  {
-    login: "thassiadevilart",
-    id: 46179885,
-    testimony:
-      "Gente é o melhor servidor dev do discord, aqui na Alemanha, na Baviera a gente não têm um servidor assim, com tanto conteúdo!",
-  },
-  {
-    login: "carolinarigaudfelix",
-    id: 83124624,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
-    login: "Manuel-Antunes",
-    id: 57446204,
-    testimony:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum massa in lorem commodo, sit amet porta arcu ullamcorper. Aenean accumsan eget elit in vulputate. Praesent rutrum ultrices arcu, id vestibulum dui fermentum vitae. Vivamus varius lorem nibh, non molestie nunc feugiat nec. Pellentesque orci velit, molestie quis arcu in, finibus scelerisque lectus. Phasellus feugiat pretium turpis, sit amet condimentum odio consectetur eget. Sed elementum sed nibh ut molestie. Vivamus augue lorem, malesuada imperdiet ante et, faucibus bibendum eros. Quisque suscipit tempor arcu sed rutrum. Etiam ultrices venenatis lobortis. Vivamus cursus, tortor ac laoreet commodo, mi libero varius purus, id malesuada felis enim at nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas interdum ipsum sed varius venenatis. Praesent vitae orci in mauris varius facilisis ut in tortor. Nam bibendum, nulla nec laoreet facilisis, lorem diam venenatis elit, et semper est lacus ut sem. Pellentesque dapibus scelerisque felis, sit amet venenatis est aliquet ac. Integer finibus, nibh non congue faucibus, dolor purus consectetur est, nec euismod neque elit vel sem. Nullam finibus tristique placerat.",
-  },
-  {
-    login: "devhenrique22",
-    id: 14983121,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
-  },
-  {
     login: "FernandoVicenteGodinho",
     id: 100453396,
     testimony:
       "Não adianta ser maomé, não vai me mover para outro servidor do discord. O Buteco dos Devs é o melhor.",
   },
   {
-    login: "juniorx57",
-    id: 130268695,
+    login: "GabriielBento",
+    id: 103751798,
     testimony:
-      "Olha esses caras, olha esses caras. Aqui é o melhor lugar pra puxar um cafézinho.",
+      "Ôh gente alguém ajuda aqui? Como que eu faço pra enviar depoimento pro site do Buteco?",
   },
   {
-    login: "CodeMoreira",
-    id: 78581400,
+    login: "brunofunnie",
+    id: 5279392,
     testimony:
-      "Amo essa comunidade descontraída e com pessoas muito legais e inteligentes! Recomendo muito para qualquer um!",
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
   },
   {
-    login: "matheusvidaltenorio",
-    id: 83835643,
+    login: "bloiseleo",
+    id: 105981050,
     testimony:
-      "Êh macho que é isso aí macho. Qual esse servidor aí macho? Éh macho esse servidor é bom viu macho.",
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "djonatah",
+    id: 22297855,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "JairAssisDev",
+    id: 63867699,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "AninhaPardini",
+    id: 108814319,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "Gus-fs",
+    id: 104308051,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "carolinarigaudfelix",
+    id: 83124624,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
+  },
+  {
+    login: "devhenrique22",
+    id: 14983121,
+    testimony:
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
   },
   {
     login: "Wadsom",
     id: 94648773,
     testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
   },
   {
     login: "nsaraiva",
     id: 398217,
     testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit. Nostrud non reprehenderit ad occaecat excepteur do veniam sint ad ex aliqua qui eu pariatur. Fugiat et commodo veniam culpa cillum irure irure labore tempor deserunt Lorem. Aliqua aute eiusmod culpa id esse amet.",
+      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing. Incididunt laborum dolor esse tempor dolore consectetur mollit.",
   },
   {
     login: "gabrielcipriano",
@@ -205,27 +222,9 @@ export const testimonies = [
       "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing.",
   },
   {
-    login: "EricksonLuiz",
-    id: 101117503,
-    testimony:
-      "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing.",
-  },
-  {
-    login: "felipepassion",
-    id: 29386600,
-    testimony:
-      "Aí lek saca só essa funcionalidade no Blazor que dá pra implementar no servidor do Buteco dos Devs, vaificar daora, serio mesmo.",
-  },
-  {
     login: "pedroosz",
     id: 48372955,
     testimony:
       "Excepteur pariatur deserunt culpa eu aliquip dolore consectetur. Laboris elit incididunt proident veniam irure aute sint. Velit aute eu incididunt non adipisicing.",
-  },
-  {
-    login: "GabriielBento",
-    id: 103751798,
-    testimony:
-      "Ôh gente alguém ajuda aqui? Como que eu faço pra enviar depoimento pro site do Buteco?",
   },
 ];

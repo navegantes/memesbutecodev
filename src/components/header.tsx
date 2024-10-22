4737ffd02d7124c7bcdc0e30097ce598c2e18605
navegantes <raphaelnavegantes@gmail.com>
2024-10-21 21:54:17 -0300

    chore: clean file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,25 +3,10 @@ import { ImGithub } from "react-icons/im";
 import { HiLightBulb } from "react-icons/hi";
 import { FaDiscord } from "react-icons/fa";
 import { Bounce, toast, ToastContainer } from "react-toastify";
-// import { useState } from "react";
 import ChipTabs from "./ChipTabs";
 // import { SlideTabs } from "./SlideTab";
-// import { SlideTabsExample } from "./SlideTab";
-// import { useLocation } from "react-router-dom";
-// import ChipTabs from "./ChipTabs";
 
 export function Header() {
-  // const [currentNavPath, setCurrentNavPath] = useState<string>("");
-  // const location = useLocation();
-  // const fullURL = window.location.href.split("/");
-  // const route = fullURL[fullURL.length - 1];
-  // console.log(location);
-
-  // const handleClick = (path: string) => {
-  //   setCurrentNavPath(path);
-  //   console.log("PATH: ", path);
-  // };
-
   return (
     <header className="flex flex-row items-center justify-center gap-6 p-4 mb-6 border-b-2 border-b-buteco-lager bg-buteco-dark">
       <div className="flex w-40 -translate-y-2 -rotate-[16deg] h-18 cursor-pointer">
@@ -29,30 +14,6 @@ export function Header() {
           <ButecoLogo />
         </a>
       </div>
-
-      {/* <nav className="flex gap-6">
-        {[
-          ["/memes", "Memes"],
-          ["/audios", "Audios"],
-          ["/videos", "Videos"],
-          ["/docs", "Documentos"],
-        ].map(([path, item]) => {
-          return (
-            <a
-              key={item}
-              href={path}
-              onClick={() => handleClick(path)}
-              className={`${
-                "/" + route === currentNavPath
-                  ? "bg-gradient-to-br from-buteco-orange to-buteco-pilsen"
-                  : ""
-              } box-content px-4 py-2 rounded-full hover:text-buteco-macchi hover:bg-buteco-cream`}
-            >
-              <span className="text-lg font-bold">{item}</span>
-            </a>
-          );
-        })}
-      </nav> */}
 
       <ChipTabs />
       {/* <SlideTabs /> */}

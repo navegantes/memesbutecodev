032fc42f5692da7d2d56e864951f8a978d42b619
navegantes <raphaelnavegantes@gmail.com>
2024-10-28 02:53:25 -0300

    feat: add lazy loading on Memes page

---
+++ src/components/MemesPage/MemeImg.tsx
@@ -0,0 +1,3 @@
+export function MemeImg({ className, src }: { className: string; src: string }) {
+  return <img className={className} src={src} />;
+}
--- src/components/MemesPage/MemesList.tsx
+++ src/components/MemesPage/MemesList.tsx
@@ -1,7 +1,12 @@
 import { Loading } from "../loading";
 import { useDataSpot } from "../../context/DataSpotContext";
+import { lazy, Suspense } from "react";
 
 const baseUrl = import.meta.env.VITE_APP_BASEURL;
+
+const MemeImg = lazy(() =>
+  import("./MemeImg").then((module) => ({ default: module.MemeImg }))
+);
 
 export function MemesList() {
   const { dataContent, isLoading, handleCLick } = useDataSpot();
@@ -21,10 +26,12 @@ export function MemesList() {
                 className="flex flex-row items-center justify-between h-36 p-4 w-[32rem] max-h-36 gap-2 border-4 border-[#6F767C] rounded-xl bg-[#07131D] hover:border-buteco-lager hover:cursor-pointer"
                 onClick={() => handleCLick(item)}
               >
-                <img
-                  className="object-cover bg-slate-200 h-full transition rounded-lg hover:scale-[2.5] hover:w-fit hover:h-fit w-full max-w-32"
-                  src={`${baseUrl}/${item}`}
-                />
+                <Suspense fallback={<Loading />}>
+                  <MemeImg
+                    className="object-cover bg-slate-200 h-full transition rounded-lg hover:scale-[2.5] hover:w-fit hover:h-fit w-full max-w-32"
+                    src={`${baseUrl}/${item}`}
+                  />
+                </Suspense>
                 <p className="flex justify-center w-full">
                   {item.split(".")[0].length > sliceSize
                     ? item.slice(0, sliceSize - 5) +
